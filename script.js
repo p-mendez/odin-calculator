82dad1bc2183ed52d33a81494e6343241184245a
p-mendez <139092055+p-mendez@users.noreply.github.com>
2024-01-05 10:44:23 -0500

    Alphabetize function list

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ function add(...numbers) {
     return numbers.reduce((previous, current) => previous + current, 0);
 }
 
-function subtract(minuend, subtrahend) {
-    return minuend - subtrahend;
+function divide(dividend, divisor) {
+    return dividend / divisor;
 }
 
 function multiply(...numbers) {
     return numbers.reduce((previous, current) => previous * current, 1);
 }
 
-function divide(dividend, divisor) {
-    return dividend / divisor;
+function subtract(minuend, subtrahend) {
+    return minuend - subtrahend;
 }

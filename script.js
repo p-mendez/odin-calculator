f3248b392de99074ed23219c74c4bc0a0d0041f1
p-mendez <139092055+p-mendez@users.noreply.github.com>
2024-01-05 10:41:17 -0500

    Complete divide  function

--- script.js
+++ script.js
@@ -9,3 +9,7 @@ function subtract(minuend, subtrahend) {
 function multiply(...numbers) {
     return numbers.reduce((previous, current) => previous * current, 1);
 }
+
+function divide(dividend, divisor) {
+    return dividend / divisor;
+}

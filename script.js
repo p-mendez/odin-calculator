b502446031fc8c970e81e26e404fa3edb3ec7b71
p-mendez <139092055+p-mendez@users.noreply.github.com>
2024-01-05 10:33:54 -0500

    Complete subtract function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function add(...numbers) {
     return numbers.reduce((previous, current) => previous + current, 0);
 }
+
+function subtract(minuend, subtrahend) {
+    return minuend - subtrahend;
+}
